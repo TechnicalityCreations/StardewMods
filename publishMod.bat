f49e75097a8223dbe2fee9d18814f4e85fbf07e7
TechnicalitiesDevelopment <176381906+TechnicalityCreations@users.noreply.github.com>
2025-08-26 10:55:00 +0100

    Fixed Batch Release Script

--- publishMod.bat
+++ publishMod.bat
@@ -1,5 +1,5 @@
 set /p "mod=Which Mod?  "
 if exist %mod%\ (
     del %mod%.zip /Q
-    "C:\Program Files\7-Zip\7z.exe" a %mod%.zip -r %mod%/* -x!*.cs -x!*.csproj -x!*obj* -x!*bin* -x!.*.png -x!*.zip x!bin x!obj
+    "C:\Program Files\7-Zip\7z.exe" a %mod%.zip -r %mod%/* -x!*.cs -x!*.csproj -x!*obj* -x!*bin* -x!.*.png -x!*.zip -x!bin -x!obj -x!*.sln
 ) else (echo "FOLDER DOESN'T EXIST")
