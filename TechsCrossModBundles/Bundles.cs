2d0e54e406fb18798123b68ac2379b144977a60d
TechnicalitiesDevelopment <176381906+TechnicalityCreations@users.noreply.github.com>
2026-08-21 11:17:56 +0100

    Populated bundles

---
+++ TechsCrossModBundles/Bundles.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechsCrossModBundles
+{
+	partial class ModEntry
+	{
+		public readonly static Bundle[] Bundles = new Bundle[] 
+		{
+			new Bundle("Crafts Room", 13, "Spring Foraging", "O 495 30", 0, -1,
+				new Pool(2,
+					new BundleItem("16"), // Horseradish
+					new BundleItem("18"), // Daff
+					new BundleItem("20"), // Leek
+					new BundleItem("22"), // Dandelion
+					new BundleItem("Bluestar", Sunberry),
+					new BundleItem("Peppercorn", Cornucopia)
+				),
+				new Pool(2,
+					new BundleItem("399"), // Spring Onion
+					new BundleItem("296"), // Salmonberry
+					new BundleItem("Lucky_Four_Leaf_Clover", SVE),
+					new BundleItem("Ridge_Cherry", RSV),
+					new BundleItem("Ridge_Azorean_Flower", RSV),
+					new BundleItem("BalmcreekCarillon", Vapius)
+				)
+			),
+			new Bundle("Crafts Room", 14, "Summer Foraging", "O 496 30", 3, -1,
+				new Pool(3,
+					new BundleItem("398"), // Grape
+					new BundleItem("396"), // Spice Berry
+					new BundleItem("402"), // Sweet Pea
+					new BundleItem("Ridge_Wild_Apple", RSV),
+					new BundleItem("Marigold", Sunberry),
+					new BundleItem("Raspberry", Cornucopia)
+				)
+			),
+			new Bundle("Crafts Room", 15, "Fall Foraging", "O 497 30", 2, -1,
+				new Pool(1,
+					new BundleItem("408"), // Hazelnut
+					new BundleItem("Mushroom_Colony", SVE),
+					new BundleItem("Lava_Lily", RSV)
+				),
+				new Pool(2,
+					new BundleItem("406"), // Wild Plum
+					new BundleItem("410"), // Blackberry
+					new BundleItem("Autumn_Drop_Berry", RSV)
+				),
+				new Pool(1,
+					new BundleItem("404") // Common Mushroom
+				)
+			),
+			new Bundle("Crafts Room", 16, "Winter Foraging", "O 498 30", 6, -1,
+				new Pool(2,
+					new BundleItem("418"), // Crocus
+					new BundleItem("414"), // Crystal Fruit
+					new BundleItem("283"), // Holly
+					new BundleItem("Sierra_Wintergreen", RSV),
+					new BundleItem("CarmineBlossom", Sunberry),
+					new BundleItem("JuniperBerries", Cornucopia)
+				),
+				new Pool(2,
+					new BundleItem("412"), // Winter Root
+					new BundleItem("416"), // Snow Yam
+					new BundleItem("Bearberrys", SVE)
+				)
+			),
+			new Bundle("Crafts Room", 19, "Exotic Foraging", "O 235 5", 1, 5,
+				new Pool(2,
+					new BundleItem("88"), // Coconut
+					new BundleItem("90") // Cactus Fruit
+				),
+				new Pool(2,
+					new BundleItem("724"), // Maple Syrup
+					new BundleItem("725"), // Oak Resin
+					new BundleItem("726"), // Pine Tar
+					new BundleItem("Fir_Wax", SVE),
+					new BundleItem("Birch_Water", SVE),
+					new BundleItem("CinderLeaf", Sunberry),
+					new BundleItem("ChicleRubber", Cornucopia),
+					new BundleItem("BirchSap", Vapius)
+				),
+				new Pool(2,
+					new BundleItem("257"), // Morel
+					new BundleItem("259"), // Fiddlehead Fern
+					new BundleItem("Poison_Mushroom", SVE),
+					new BundleItem("Smelly_Rafflesia", SVE)
+				),
+				new Pool(2,
+					new BundleItem("78"), // Cave Carrot
+					new BundleItem("394"), // Rainbow Shell
+					new BundleItem("Thistle", SVE),
+					new BundleItem("Mountain_Mistbloom", RSV)
+				)
+			),
+
+			new Bundle("Pantry", 0, "Spring Crops", "O 465 20", 0, -1,
+				new Pool(4,
+					new BundleItem("24"), // Parsnip
+					new BundleItem("188"), // Green Bean
+					new BundleItem("190"), // Cauliflower
+					new BundleItem("192"), // Potato
+					new BundleItem("250"), // Kale
+					new BundleItem("Carrot"),
+					new BundleItem("Cucumber", SVE),
+					new BundleItem("SugarBeet", Cornucopia),
+					new BundleItem("Asparagus", Cornucopia),
+					new BundleItem("Buckwheat", Cornucopia),
+					new BundleItem("Cabbage", Cornucopia),
+					new BundleItem("Onion", Cornucopia),
+					new BundleItem("Spinach", Cornucopia)
+				)
+			),
+			new Bundle("Pantry", 1, "Summer Crops", "O 621 1", 3, -1,
+				new Pool(4,
+					new BundleItem("256"), // Tomato
+					new BundleItem("260"), // Hot Pepper
+					new BundleItem("258"), // Blueberry
+					new BundleItem("254"), // Melon
+					new BundleItem("SummerSquash"),
+					new BundleItem("Butternut_Squash", SVE),
+					new BundleItem("Sunberries", Sunberry),
+					new BundleItem("BellPepper", Cornucopia),
+					new BundleItem("Honeydew", Cornucopia),
+					new BundleItem("GreenPeas", Cornucopia),
+					new BundleItem("Watermelon", Cornucopia),
+					new BundleItem("Chickpea", Cornucopia)
+				)
+			),
+			new Bundle("Pantry", 2, "Fall Crops", "BO 10 1", 2, -1,
+				new Pool(4,
+					new BundleItem("270"), // Corn
+					new BundleItem("272"), // Eggplant
+					new BundleItem("276"), // Pumpkin
+					new BundleItem("280"), // Yam
+					new BundleItem("Broccoli"),
+					new BundleItem("282"), // Cranberry
+					new BundleItem("Sweet_Potato", SVE),
+					new BundleItem("Barley", Cornucopia),
+					new BundleItem("Zucchini", Cornucopia),
+					new BundleItem("Celery", Cornucopia),
+					new BundleItem("Lentils", Cornucopia),
+					new BundleItem("Oats", Cornucopia),
+					new BundleItem("Turnip", Cornucopia)
+				)
+			),
+			new Bundle("Pantry", 3, "Quality Crops", "BO 15 1", 6, 3,
+				// Spring
+				new Pool(1, 2, 5,
+					new BundleItem("24"), // Parsnip
+					new BundleItem("188"), // Green Bean
+					new BundleItem("190"), // Cauliflower
+					new BundleItem("192"), // Potato
+					new BundleItem("250"), // Kale
+					new BundleItem("Carrot"),
+					new BundleItem("Cucumber", SVE),
+					new BundleItem("SugarBeet", Cornucopia),
+					new BundleItem("Asparagus", Cornucopia),
+					new BundleItem("Buckwheat", Cornucopia),
+					new BundleItem("Cabbage", Cornucopia),
+					new BundleItem("Onion", Cornucopia),
+					new BundleItem("Spinach", Cornucopia)
+				),
+				//Summer
+				new Pool(1, 2, 5,
+					new BundleItem("256"), // Tomato
+					new BundleItem("260"), // Hot Pepper
+					new BundleItem("258"), // Blueberry
+					new BundleItem("254"), // Melon
+					new BundleItem("SummerSquash"),
+					new BundleItem("Butternut_Squash", SVE),
+					new BundleItem("Sunberries", Sunberry),
+					new BundleItem("BellPepper", Cornucopia),
+					new BundleItem("Honeydew", Cornucopia),
+					new BundleItem("GreenPeas", Cornucopia),
+					new BundleItem("Watermelon", Cornucopia),
+					new BundleItem("Chickpea", Cornucopia)
+				),
+				//Fall
+				new Pool(1, 2, 5,
+					new BundleItem("270"), // Corn
+					new BundleItem("272"), // Eggplant
+					new BundleItem("276"), // Pumpkin
+					new BundleItem("280"), // Yam
+					new BundleItem("Broccoli"),
+					new BundleItem("282"), // Cranberry
+					new BundleItem("Sweet_Potato", SVE),
+					new BundleItem("Barley", Cornucopia),
+					new BundleItem("Zucchini", Cornucopia),
+					new BundleItem("Celery", Cornucopia),
+					new BundleItem("Lentils", Cornucopia),
+					new BundleItem("Oats", Cornucopia),
+					new BundleItem("Turnip", Cornucopia)
+				)
+			),
+			new Bundle("Pantry", 4, "Animal", "BO 12 1", 4, 5,
+				new Pool(3,
+					new BundleItem("174"), // Large White Egg
+					new BundleItem("182"), // Large Brown Egg
+					new BundleItem("442"), // Duck Egg
+					new BundleItem("LargeQuailEgg", "Mizu.Quail"),
+					new BundleItem("GooseEgg", "Mizu.Goose")
+				),
+				new Pool(2, 
+					new BundleItem("186"), // Large Milk
+					new BundleItem("438") // Large Goat Milk
+				),
+				new Pool(1,
+					new BundleItem("440"), // Wool
+					new BundleItem("SpeckledFowlEgg", Vapius)
+				)
+			),
+			new Bundle("Pantry", 5, "Artisan", "BO 10 1", 1, 6,
+				new Pool(1,
+					new BundleItem("432"), // Truffle Oil
+					new BundleItem("428"), // Cloth
+					new BundleItem("TruffleMayonnaise", Cornucopia),
+					new BundleItem("BlueVapiusCheese", Vapius),
+					new BundleItem("EweCheese", Vapius)
+				),
+				new Pool(1,
+					new BundleItem("307") // Duck Mayo
+				),
+				new Pool(1, 
+					new BundleItem("426"), // Goat Cheese
+					new BundleItem("SkyshardSharpCheese")
+				),
+				new Pool(3,
+					new BundleItem("613"), // Apple
+					new BundleItem("634"), // Apricot
+					new BundleItem("635"), // Orange
+					new BundleItem("636"), // Peach
+					new BundleItem("637"), // Pom
+					new BundleItem("638"), // Cherry
+					new BundleItem("Nectarine", SVE),
+					new BundleItem("Pear", SVE),
+					new BundleItem("Cherry_Pluot", RSV),
+					new BundleItem("Highland_Jostaberry", RSV),
+					new BundleItem("Mountain_Plumcot", RSV),
+					new BundleItem("Northern_Limequat", RSV),
+					new BundleItem("Paradise_Rangpur", RSV),
+					new BundleItem("Tropi_Ugli_Fruit", RSV),
+					new BundleItem("Lemon", Cornucopia),
+					new BundleItem("Lime", Cornucopia),
+					new BundleItem("Grapefruit", Cornucopia),
+					new BundleItem("Pomelo", Cornucopia),
+					new BundleItem("Fig", Cornucopia),
+					new BundleItem("Yuzu", Cornucopia)
+					),
+				new Pool(3,
+					new BundleItem("344"), // Jelly
+					new BundleItem("342"), // Pickles
+					new BundleItem("348"), // Wine
+					new BundleItem("350"), // Juice
+					new BundleItem("DriedFlower", Cornucopia),
+					new BundleItem("DriedHerb", Cornucopia),
+					new BundleItem("Candy_CandiedFruit", Vapius),
+					new BundleItem("Candy_CandiedFlower", Vapius)
+					),
+				new Pool(2,
+					new BundleItem("340"), // Honey
+					new BundleItem("DriedMushrooms"),
+					new BundleItem("RoyalJelly", Cornucopia)
+				)
+			),
+
+			new Bundle("Fish Tank", 6, "River Fish", "O 685 30", 6, -1,
+				new Pool(3,
+					new BundleItem("145"), // Sunfish
+					new BundleItem("706"), // Shad
+					new BundleItem("699"), // Tiger Trout
+					new BundleItem("Minnow", SVE)
+				),
+				new Pool(1,
+					new BundleItem("143") // Catfish
+				)
+			),
+			new Bundle("Fish Tank", 7, "Lake Fish", "O 687 1", 0, -1,
+				new Pool(3,
+					new BundleItem("136"), // Largemouth Bass
+					new BundleItem("142"), // Carp
+					new BundleItem("700"), // Bullhead
+					new BundleItem("Tadpole", SVE),
+					new BundleItem("Bull_Trout", SVE)
+				),
+				new Pool(1,
+					new BundleItem("698") // Sturgeon
+				)
+			),
+			new Bundle("Fish Tank", 8, "Ocean Fish", "O 690 5", 5, -1,
+				new Pool(4,
+					new BundleItem("131"), // Sardine
+					new BundleItem("130"), // Tuna
+					new BundleItem("150"), // Red Snapper
+					new BundleItem("701"), // Tilapia
+					new BundleItem("Starfish", SVE),
+					new BundleItem("SeaTurtle", ASF),
+					new BundleItem("Marlin", ASF)
+				)
+			),
+			new Bundle("Fish Tank", 9, "Night Fishing", "R 516 1", 1, -1,
+				new Pool(3,
+					new BundleItem("132"), // Bream
+					new BundleItem("140"), // Walleye
+					new BundleItem("148"), // Eel
+					new BundleItem("269"), // Midnight Carp
+					new BundleItem("Slime_Barbel", ES),
+					new BundleItem("FiretailGuppy", Sunberry)
+				)
+			),
+			new Bundle("Fish Tank", 10, "Specialty Fishing", "O 242 5", 4, -1,
+				new Pool(4,
+					new BundleItem("128"), // Pufferfish
+					new BundleItem("156"), // Ghostfish
+					new BundleItem("164"), // Sandfish
+					new BundleItem("734"), // Woodskip
+					new BundleItem("Radioactive_Bass", SVE),
+					new BundleItem("Ridge_Bluegill", RSV),
+					new BundleItem("LeafyTrout", Vapius),
+					new BundleItem("MarineGold", "DTZ.DowntownZuzuCP"),
+					new BundleItem("MossyEel", ASF),
+					new BundleItem("JackOFish", ASF)
+				)
+			),
+
+			new Bundle("Bulletin Board", 31, "Chef's", "O 221 3", 4, -1,
+				new Pool(2,
+					new BundleItem("724"), // M Syrup
+					new BundleItem("259"), // F Fern
+					new BundleItem("Birch_Syrup", SVE),
+					new BundleItem("JuniperBerries", Cornucopia),
+					new BundleItem("Sugarcane", Cornucopia)
+				),
+				new Pool(2,
+					new BundleItem("430"), // Truffle
+					new BundleItem("376"), // Poppy
+					new BundleItem("Gold_Carrot", SVE),
+					new BundleItem("Chives", Cornucopia),
+					new BundleItem("Parsley", Cornucopia),
+					new BundleItem("Olive", Vapius)
+				),
+				new Pool(2,
+					new BundleItem("194"), // Fried Egg
+					new BundleItem("228"), // Maki
+					new BundleItem("Frog_Legs", SVE),
+					new BundleItem("Glazed_Butterfish", SVE),
+					new BundleItem("Arugula_Roll", RSV),
+					new BundleItem("ChocolatePudding", Cornucopia),
+					new BundleItem("Cooking_Quiche", Vapius)
+				)
+			),
+			new Bundle("Bulletin Board", 34, "Dye", "BO 25 1", 6, -1,
+				new Pool(1,
+					new BundleItem("420"), // Red Mushroom
+					new BundleItem("Sweet_Potato", SVE),
+					new BundleItem("Paneeki", ES),
+					new BundleItem("Geranium", Cornucopia)
+					),
+				new Pool(1,
+					new BundleItem("396"), // Spice Berry
+					new BundleItem("Red_Baneberry", SVE),
+					new BundleItem("Lava_Lily", RSV)
+					),
+				new Pool(1,
+					new BundleItem("421"), // Sunflower
+					new BundleItem("Forest_Amancay", RSV),
+					new BundleItem("Marigold", Sunberry)
+					),
+				new Pool(1,
+					new BundleItem("444"), // Duck Feather
+					new BundleItem("Aloe", Cornucopia)
+					),
+				new Pool(1,
+					new BundleItem("62"), // Aquamarine
+					new BundleItem("Mountain_Hokkaido", RSV),
+					new BundleItem("Azure_Chrysanthemum", ES),
+					new BundleItem("BlueMist", Cornucopia)
+					),
+				new Pool(1,
+					new BundleItem("397"), // Sea Urchin
+					new BundleItem("ZinfazuFruit", ES),
+					new BundleItem("Sunberries", Sunberry),
+					new BundleItem("Clematis", Cornucopia),
+					new BundleItem("PinkMorningGlory", Vapius)
+					)
+			),
+			new Bundle("Bulletin Board", 32, "Field Research", "BO 20 1", 5, -1,
+				new Pool(2,
+					new BundleItem("422"), // Purple Mushroom
+					new BundleItem("392"), // Nautilus Shell
+					new BundleItem("Lucky_Four_Leaf_Clover", SVE),
+					new BundleItem("Amber", SVE),
+					new BundleItem("Violet_Devil_s_Claw", RSV),
+					new BundleItem("ChickenoftheWoods", Cornucopia),
+					new BundleItem("WoodSilkFlower", Vapius)
+				),
+				new Pool(1,
+					new BundleItem("702"), // Chub
+					new BundleItem("Puppyfish", SVE),
+					new BundleItem("Caped_Tree_Frog", RSV),
+					new BundleItem("Obsidian_Maw", ES),
+					new BundleItem("BubblyBetta", Sunberry),
+					new BundleItem("CrystalFish", Vapius),
+					new BundleItem("HermitCrab", ASF)
+				),
+				new Pool(1,
+					new BundleItem("536"), // F Geode
+					new BundleItem("SunberryGeode", Sunberry),
+					new BundleItem("BlackGeode", Vapius)
+				)
+			),
+			new Bundle("Bulletin Board", 35, "Fodder", "BO 104 1", 3, -1,
+				new Pool(1,
+					new BundleItem("262", count: 10), // Wheat
+					new BundleItem("Buckwheat", Cornucopia, count: 10)
+				),
+				new Pool(1,
+					new BundleItem("178", count: 10), // Hay
+					new BundleItem("selph.CoopFeed.ChickenFeed", count: 20)
+				),
+				new Pool(1,
+					new BundleItem("613", count:3)
+				)
+			),
+
+			new Bundle("Abandoned Joja Mart", 36, "The Missing", "", 1, 5,
+				new Pool(6,
+					new BundleItem("348", quality: 1), // Wine
+					new BundleItem("807"), // Dino Mayo
+					new BundleItem("74", quality: 2), // Prismatic Shard
+					new BundleItem("454", quality: 2, count: 5), // Ancient Fruit
+					new BundleItem("795", quality: 2), // Void Salmon
+					new BundleItem("445"), // Caviar
+					new BundleItem("162"), // Lava Eel
+					new BundleItem("Swirl_Stone", SVE),
+					new BundleItem("Aged_Blue_Moon_Wine", SVE),
+					new BundleItem("Hundred_Flavor_Doughnut", RSV),
+					new BundleItem("LunarBean", Sunberry)
+				)
+			)
+		};
+	}
+}
--- TechsCrossModBundles/ModEntry.cs
+++ TechsCrossModBundles/ModEntry.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework;
 
 namespace TechsCrossModBundles
 {
-    public sealed class ModEntry : Mod
+    public sealed partial class ModEntry : Mod
     {
 		static Dictionary<string, string> bundles;
 		internal new static IModHelper Helper;
@@ -16,51 +16,9 @@ namespace TechsCrossModBundles
 			RSV = "Rafseazz.RSVCP",
 			Vapius = "Lumisteria.MtVapius",
 			Cornucopia = "Cornucopia",
+			ES = "EastScarp",
+			ASF = "ASF",
 			Sunberry = "skellady.SBVCP";
-		public readonly static Bundle[] Bundles = new Bundle[]
-		{
-			new Bundle("Crafts Room", 13, "Spring Foraging", "O 495 30", 0, -1,
-				//Anywhere Pool
-				new Pool(2, new BundleItem("16"), new BundleItem("18"), new BundleItem("20"), new BundleItem("22"), new BundleItem("Bluestar", Sunberry), new BundleItem("Peppercorn", Cornucopia)),
-				// Location/Time Specific
-				new Pool(2, new BundleItem("399"), new BundleItem("296"), new BundleItem("Lucky_Four_Leaf_Clover", SVE), new BundleItem("Ridge_Cherry", RSV), new BundleItem("Ridge_Azorean_Flower", RSV), new BundleItem("BalmcreekCarillon", Vapius))),
-			new Bundle("Crafts Room", 14, "Summer Foraging", "O 496 30", 3, -1,
-				new Pool(3, new BundleItem("398"), new BundleItem("396"), new BundleItem("402"), new BundleItem("Ridge_Wild_Apple", RSV), new BundleItem("Marigold", Sunberry), new BundleItem("Raspberry", Cornucopia))
-				),
-			new Bundle("Crafts Room", 15, "Fall Foraging", "O 497 30", 2, -1,
-				//Main
-				new Pool(1, new BundleItem("408"), new BundleItem("Mushroom_Colony", SVE), new BundleItem("Lava_Lily", RSV)),
-				// Fruit
-				new Pool(2, new BundleItem("406"), new BundleItem("410"), new BundleItem("Autumn_Drop_Berry", RSV)),
-				// Mushroom
-				new Pool(1, new BundleItem("404"))
-				),
-			new Bundle("Crafts Room", 16, "Winter Foraging", "O 498 30", 6, -1,
-				// Main
-				new Pool(2, new BundleItem("418"), new BundleItem("414"), new BundleItem("283"), new BundleItem("Sierra_Wintergreen", RSV), new BundleItem("CarmineBlossom", Sunberry), new BundleItem("JuniperBerries", Cornucopia)),
-				// Tilling
-				new Pool(2, new BundleItem("412"), new BundleItem("416"), new BundleItem("Bearberrys", SVE))
-				),
-			new Bundle("Crafts Room", 17, "Construction", "BO 114 1", 4, -1,
-				new VanillaPool(4, new BundleItem("388", count:99), new BundleItem("388", count:99), new BundleItem("390", count:99), new BundleItem("709", count:10))),
-			new Bundle("Crafts Room", 19, "Exotic Foraging", "O 235 5", 1, 5, 
-				//Desert
-				new Pool(2, new BundleItem("88"), new BundleItem("90")),
-				//Trees
-				new Pool(2, new BundleItem("724"), new BundleItem("725"), new BundleItem("726"), new BundleItem("Fir_Wax", SVE), new BundleItem("Birch_Water", SVE), new BundleItem("CinderLeaf", Sunberry), new BundleItem("ChicleRubber", Cornucopia), new BundleItem("BirchSap", Vapius)),
-				//Secret Woods
-				new Pool(2, new BundleItem("257"), new BundleItem("259"), new BundleItem("Poison_Mushroom", SVE), new BundleItem("Smelly_Rafflesia", SVE)),
-				//Other
-				new Pool(2, new BundleItem("78"), new BundleItem("394"), new BundleItem("Thistle", SVE), new BundleItem("Mountain_Mistbloom", RSV))
-				),
-			new Bundle("Pantry", 0, "Spring Crops", "O 465 20", 0, -1,
-				new Pool(4, new BundleItem("24"), new BundleItem("188"), new BundleItem("190"), new BundleItem("192"), new BundleItem("250"), new BundleItem("Carrot"),
-					new BundleItem("Cucumber", SVE),
-					new BundleItem("SugarBeet", Cornucopia), new BundleItem("Asparagus", Cornucopia), new BundleItem("Buckwheat", Cornucopia), new BundleItem("Cabbage", Cornucopia), new BundleItem("Onion", Cornucopia), new BundleItem("Spinach", Cornucopia))
-				),
-			new Bundle("Pantry", 1, "Summer Crops", "O 621 1", 3, -1,
-				new Pool(4, new BundleItem("256"), new BundleItem("260"), new BundleItem("258"), new BundleItem("254"), new BundleItem("SummerSquash"), new BundleItem("Butternut_Squash", SVE), new BundleItem("Sunberries", Sunberry), new BundleItem("BellPepper", Cornucopia), new BundleItem("Honeydew", Cornucopia), new BundleItem("GreenPeas", Cornucopia), new BundleItem("Watermelon", Cornucopia), new BundleItem("Chickpea", Cornucopia)))
-		};
 		public override void Entry(IModHelper helper)
 		{
 			Helper = helper;
@@ -73,7 +31,7 @@ namespace TechsCrossModBundles
 
 		private static void LoadBundlesVar(object? sender = null, StardewModdingAPI.Events.SaveLoadedEventArgs e = null)
 		{
-			bundles = Helper.Data.ReadSaveData<Dictionary<string, string>>("TCB");
+			bundles = Helper.Data.ReadSaveData<Dictionary<string, string>>("TCMB");
 			if (bundles == null) GenerateBundles(null);
 			else SetGame1Bundles();
 
@@ -84,27 +42,33 @@ namespace TechsCrossModBundles
 		}
 		private void SaveBundles(object? sender, StardewModdingAPI.Events.SavingEventArgs e)
 		{
-			Helper.Data.WriteSaveData("TCB", bundles);
+			Helper.Data.WriteSaveData("TCMB", bundles);
 		}
 
 		private static void GenerateBundles(object? sender, StardewModdingAPI.Events.SaveCreatedEventArgs e = null)
 		{
-			Console.WriteLine("[Tech's Cross Mod Bundles] Generating bundles");
+			Console.WriteLine("[TCMB] Generating bundles");
 			bundles = new Dictionary<string, string>();
 			foreach (var b in Bundles)
 			{
 				var g = b.Generate();
 				bundles.Add(g.Key, g.Value);
 			}
-			var vault = new Dictionary<string, string>
+			var staticbundles = new Dictionary<string, string>
 			{
+				{"Fish Tank/11", "Crab Pot/O 710 3/715 1 0 716 1 0 717 1 0 718 1 0 719 1 0 720 1 0 721 1 0 722 1 0 723 1 0 372 1 0/1/5" },
 				{"Vault/23", "2,500g/O 220 3/-1 2500 2500/4"},
 				{"Vault/24", "5,000g/O 369 30/-1 5000 5000/2"},
 				{"Vault/25", "10,000g/BO 9 1/-1 10000 10000/3"},
 				{"Vault/26", "25,000g/BO 21 1/-1 25000 25000/1"},
+				{"Boiler Room/20", "Blacksmith's/BO 13 1/334 1 0 335 1 0 336 1 0/2"},
+				{"Boiler Room/21", "Geologist's/O 749 5/80 1 0 86 1 0 84 1 0 82 1 0/1"},
+				{"Boiler Room/22", "Adventurer's/R 518 1/766 99 0 767 10 0 768 1 0 769 1 0/1/2"},
+				{"Bulletin Board/33", "Enchanter's/O 336 5/725 1 0 348 1 0 446 1 0 637 1 0/1"},
+				{"Crafts Room/17", "Construction/BO 114 1/388 99 0 388 99 0 390 99 0 709 10 0/4" },
 			};
-			bundles.TryAddMany(vault);
-			Console.WriteLine("Generated Bundles");
+			bundles.TryAddMany(staticbundles);
+			Console.WriteLine("[TCMB] Generated Bundles");
 			foreach (var l in bundles) Console.WriteLine($"\"{l.Key}\": \"{l.Value}\"");
 			SetGame1Bundles();
 		}
@@ -139,6 +103,7 @@ namespace TechsCrossModBundles
 			}
 			public KeyValuePair<string, string> Generate()
 			{
+				Console.WriteLine($"[TCMB] Generating {Name} Bundle");
 				var requirements = new List<BundleItem>();
 				foreach(var p in Pools)
 				{
@@ -148,6 +113,8 @@ namespace TechsCrossModBundles
 				var key = $"{Room}/{SpriteIdx}";
 				var value = $"{Name}/{Reward}/{string.Join(' ', requirements)}/{Colour}/{RequiredCount}";
 				return new KeyValuePair<string, string>(key, value);
+				Console.WriteLine($"[TCMB] Generated {Name} Bundle");
+
 			}
 		}
 		public class BundleItem
@@ -156,12 +123,12 @@ namespace TechsCrossModBundles
 			public string ID;
 			public int Count;
 			public int MinQuality;
-			public BundleItem(string id, string mod = "", int count = 1, int minQuality = 0)
+			public BundleItem(string id, string mod = "", int count = 1, int quality = 0)
 			{
 				Mod = mod;
 				ID = (mod==""? "":mod + "_") +id;
 				Count = count;
-				MinQuality = minQuality;
+				MinQuality = quality;
 			}
 			public override string ToString()
 			{
@@ -211,6 +178,16 @@ namespace TechsCrossModBundles
 				{
 				Choose:
 					var item = Game1.random.ChooseFrom(Items);
+					if(Items.Count == 0)
+					{
+						throw new Exception("Items was empty");
+					}
+					if(item == null)
+					{
+						Console.WriteLine("Item was null");
+						goto Choose;
+					}
+					Console.WriteLine("[TCMB] Attempting to select " + item.ID);
 					if (!ItemRegistry.Exists(item.ID)) continue;
 					if (item.Mod == lastMod && !onlyOneModLeft)
 					{
@@ -225,6 +202,8 @@ namespace TechsCrossModBundles
 						}
 						if (!onlyOneModLeft) goto Choose;
 					}
+					Console.WriteLine("[TCMB] Chose " + item.ID);
+
 					returnMe.Add(item);
 					Items.Remove(item);
 					lastMod = item.Mod;
@@ -237,6 +216,11 @@ namespace TechsCrossModBundles
 			if (ItemRegistry.Exists(b.ID))
 			{
 				list.Add(b);
+				Console.WriteLine("[TCMB] Adding " + b.ID + " to pool");
+			}
+			else
+			{
+				Console.WriteLine("[TCMB] Skipping " + b.ID);
 			}
 		}
 
